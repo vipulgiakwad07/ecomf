2caf928181e24d8970631aa2598f17dac3243dba
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-06-01 15:39:38 +0530

    Delete client directory

--- client/src/Components/default.js
+++
@@ -1,4 +0,0 @@
-// export { default as Cart } from './Cart';
-export { default as Home } from './Home';
-// export { default as Header} from './Header';
-export { default as NotFound } from './NotFound';
